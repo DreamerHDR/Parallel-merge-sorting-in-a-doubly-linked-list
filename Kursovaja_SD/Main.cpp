7b67c48b2799591d6912f8c6283f6b52d7bb44d3
DreamerHDR <107370928+DreamerHDR@users.noreply.github.com>
2024-03-18 21:07:24 +0300

    Update Main.cpp

--- Kursovaja_SD/Main.cpp
+++ Kursovaja_SD/Main.cpp
@@ -2,17 +2,19 @@
 
 int main() {
     system("chcp 1251");
-    DoublyLinkedList<int> list;
+    DoublyLinkedList<float> list;
 
-    list.insertBack(12);
-    list.insertBack(11);
-    list.insertBack(13);
-    list.insertBack(5);
-    list.insertBack(6);
-    list.insertBack(7);
 
-    list.display();
-    list.choice();
+   //list.insertBack(12);
+   //list.insertBack(11);
+   //list.insertBack(13);
+   //list.insertBack(5);
+   //list.insertBack(6);
+   //list.insertBack(7);
+
+   //list.display();
+   list.choiceHelper();
+
 
     return 0;
 }
