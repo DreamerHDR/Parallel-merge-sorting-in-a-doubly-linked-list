2881f4cc475fafd25306a596466a43e768cddcd4
DreamerHDR <107370928+DreamerHDR@users.noreply.github.com>
2024-03-18 21:07:03 +0300

    Update DoublyLinkedList.h

--- Kursovaja_SD/DoublyLinkedList.h
+++ Kursovaja_SD/DoublyLinkedList.h
@@ -4,6 +4,13 @@
 #include <fstream>
 #include <thread>
 #include <mutex>
+#include <ctime>
+#include <cstdlib>
+#include <type_traits>
+#include <random>
+#include <chrono> 
+#include <vector>
+#include <algorithm>
 
 template <typename T>
 class Node {
@@ -29,14 +36,25 @@ private:
 public:
     DoublyLinkedList();
     ~DoublyLinkedList();
+
     void insertBack(T val);
     void insertFront(T val);
     void insertAfter(Node<T>* prevNode, T val);
     Node<T>* getHead() const;
+
     void display();
     void parallelMergeSort();
     void saveToFile(const std::string& filename);
     void loadFromFile(const std::string& filename);
     void removeAt(int index);
-    void choice();
+    void clear();
+    T sum() const;
+
+    void generateRandomList(int size);
+    void generateUnsortedRandomList(int length);
+    void generateHalfSortedRandomList(int length);
+
+    void compareSortingTime();
+    void displayList(Node<T>* listHead);
+    void choiceHelper();
 };
